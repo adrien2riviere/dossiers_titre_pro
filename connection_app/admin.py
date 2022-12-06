86b599b949709a0a9e5cf9d859e32c5c0e0cc7a6
Adrien <107502036+adrien2riviere@users.noreply.github.com>
2022-12-06 21:17:01 +0100

    Delete connection_app directory

--- connection_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from connection_app.models import User
-
-admin.site.register(User)
--- connection_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConnectionAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'connection_app'
