22d57d40221458f2352d5f19ac590b9d5a6e759f
Adrien <107502036+adrien2riviere@users.noreply.github.com>
2022-12-06 21:17:13 +0100

    Delete contacts_app directory

--- contacts_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from contacts_app.models import Contact, Network, Party
-
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(Network)
-admin.site.register(Party)
--- contacts_app/static/editContact.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('id_profile_photo').onchange = function () {
-    var src = URL.createObjectURL(this.files[0])
-    document.getElementById('image').src = src
-}
